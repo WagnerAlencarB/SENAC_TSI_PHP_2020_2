20d46979ef30616a02db72fcb09bba262ae5dd53
WagnerAlencarB <wagneralencar94@hotmail.com>
2020-09-24 19:19:37 -0300

    aula 17/9

---
+++ nano_app/gravacontatotpl.php
@@ -0,0 +1,12 @@
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+       <p> <?php echo $msg ; ?> </p>
+        <a href='gravacontato.php'>Voltar</a>
+</body>
+</html>
